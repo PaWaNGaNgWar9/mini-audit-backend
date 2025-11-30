1722a9a0def66326f118fe82cb152776b8f8b6cd
PaWaNGaNgWar9 <pawangangwar073@gmail.com>
2025-11-30 14:39:22 +0530

    Initial commit - backend

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-
 const app = express();
 app.use(cors());
 app.use(express.json());
